b158093e336fcc93b0f595400ed8a625b33f1a7e
brendanmcnally22 <brendanmcnally295@gmail.com>
2025-05-06 22:03:17 -0400

    Ayee

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,3 +1,4 @@
+using Unity.Jobs;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -7,10 +8,19 @@ public class Movement : MonoBehaviour
     // Making Movement again
     public float speed = 10f;
     public float health = 100f;
+    public float jumpforce = 7f;
+    public float dashSpeed = 5f;
+    public float dashDuration = 0.2f;
+    public LayerMask groundLayer; // a Layer mask is used to help specify which layer you want to collide with 
+    private bool isGrounded;
+    private bool isDashing = false;
+    private float dashTime;
+    private Rigidbody2D rb;
     private Animator animator;
 
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
         animator = GetComponent<Animator>();
 
     }
@@ -18,38 +28,100 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+
+
         MovementFunction(); // Calling player Movement here
         AnimationFunction();
+        HandleDash();
+        Jump();
+        Debug.Log("isGrounded: " + isGrounded);
     }
 
     void AnimationFunction()
     {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-
-        bool isWalking = horizontal != 0 || vertical != 0;
+        bool isWalking = Mathf.Abs(rb.linearVelocity.x) > 0.1f; // check if moving happens, if it does then trigger walking animation (basically is the velocity more than 0.1f if so walk)
 
         animator.SetBool("isWalking", isWalking);
+
     }
     void MovementFunction()
     {
 
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.position += Vector3.up * speed * Time.deltaTime; 
-        }
-
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.position += Vector3.down * speed * Time.deltaTime;
-        }
-
-       if (Input.GetKey(KeyCode.D))
-        {
-            transform.position -= Vector3.left * speed * Time.deltaTime;
-        }
+        float moveX = 0f;
 
         if (Input.GetKey(KeyCode.A))
-        { transform.position -= Vector3.right * speed * Time.deltaTime; }
+        {
+            moveX = -1f;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            moveX = 1f;
+        }
+
+        if (moveX != 0)
+        {
+            transform.localScale = new Vector3(Mathf.Sign(moveX), 1f, 1f);
+        }
+
+        rb.linearVelocity = new Vector2(moveX * speed, rb.linearVelocity.y);
     }
-}
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+       if ((( 1 << collision.gameObject.layer) & groundLayer) !=0)
+        {
+            isGrounded = true;
+        }
+
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (((1 << collision.gameObject.layer) & groundLayer) != 0)
+        {
+            isGrounded = false;
+
+        }
+    }
+        void HandleDash()
+        {
+
+            if (isDashing)
+            {
+                dashTime -= Time.deltaTime;
+                if (dashTime <= 0)
+                {
+                    isDashing = false;
+                    rb.linearVelocity = Vector2.zero;
+                }
+            }
+
+            if (Input.GetKeyDown(KeyCode.LeftShift) && isGrounded)
+            {
+                StartDash();
+
+            }
+        }
+
+        void StartDash()
+        {
+            isDashing = true;
+            dashTime = dashDuration;
+
+            float direction = Input.GetKey(KeyCode.A) ? -1 : 1;
+            rb.linearVelocity = new Vector2(direction * dashSpeed, 0f);
+        }
+
+        void Jump()
+        {
+
+            if (Input.GetKey(KeyCode.Space) && isGrounded)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpforce);
+            }
+        }
+    }
+
+
+
